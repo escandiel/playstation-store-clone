ba27a25c3437ef9e24a2fbfaa25a437634272092
Victor Escandiel <joaoescandielb@gmail.com>
2023-09-18 21:03:33 -0300

    editing the CSS files

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  photoCover: string = '/assets/spider-man-2.png';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
